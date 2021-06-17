fb4d88905fe6d25a7feb7ca0a4de5629a5530dff
Lithos <edmogos89@gmail.com>
2021-06-17 13:00:34 -0600

    Added quiz game from Moiz and migrated basic functionality of the game to Vaadin

--- cpsc-329-group-project/src/main/java/com/cpsc329/web_quiz/sections/Home.java
+++ cpsc-329-group-project/src/main/java/com/cpsc329/web_quiz/sections/Home.java
@@ -1,6 +1,7 @@
 package com.cpsc329.web_quiz.sections;
 
 import com.cpsc329.web_quiz.MainView;
+import com.cpsc329.web_quiz.Quiz;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.Route;
@@ -9,6 +10,6 @@ import com.vaadin.flow.router.Route;
 public class Home extends Div {
 	
 	public Home() {
-		add(new H1("Test"));
+		add(new Quiz());
 	}
 }
